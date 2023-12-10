474d3caa6c5a6eecd36281ab55b6a05e84acfc54
nguyen tuan hiep <hiepnt8703@gmail.com>
2023-12-11 03:40:57 +0700

    layout

---
+++ layout/header.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
