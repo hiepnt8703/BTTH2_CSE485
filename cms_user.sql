b2efdc1e36446f2bf02ae387fa01b636bfac2008
Luong Manh Kien <luongmanhkien155@gmail.com>
2023-12-11 21:52:51 +0700

    kienn push

--- cms_user.sql
+++ cms_user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2019 at 09:50 AM
+-- Generation Time: Dec 10, 2023 at 09:50 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
