a13243d03f1d31b72d21e9a4f941ab6ccc1d0e22
unknown <imubslayer10@gmail.com>
2023-12-11 23:19:36 +0700

    test

--- layout/container.php
+++ layout/container.php
@@ -5,7 +5,7 @@
         
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="">Home</a></li>
+            <li class="active"><a href="#">Home</a></li>
            
           </ul>
          
